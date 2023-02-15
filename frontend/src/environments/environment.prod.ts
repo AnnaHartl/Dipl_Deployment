100a05ba24d1e5421358ce34be6ff42cf0b8c450
AnnaHartl <anna.sofie.hartl@gmail.com>
2023-02-15 21:44:42 +0100

    fixed image

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiaGF1dHp5IiwiYSI6ImNsNXNzdGdidzA3cWUzaXQ4b25oMm5kcngifQ.YrWFM35R5Gyldf-I4nU4WQ'
+    accessToken: ''
   }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiaGF1dHp5IiwiYSI6ImNsNXNzdGdidzA3cWUzaXQ4b25oMm5kcngifQ.YrWFM35R5Gyldf-I4nU4WQ'
+    accessToken: ''
   }
 };
 
